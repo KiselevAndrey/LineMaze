14dd10af879a5221761402e22b1772fd49949a27
Andrey <andrey-199213@mail.ru>
2021-07-31 20:00:42 +0300

    first blocks test

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    [Header("Blocks")]
+    [SerializeField] private GameObject standartBlock;
+
+    [Header("Parameters")]
+    [SerializeField, Min(5), Tooltip("How many lines of blocks are there at the start")] private int startCountBlocksLines;
+    [SerializeField, Min(5), Tooltip("How fast are the blocks falling")] private float fallingSpeed;
+    [SerializeField, Min(0), Tooltip("How many lines to add")] private int howManyLinesToAdd;
+    [SerializeField, Min(0), Tooltip("How fast the speed increases")] private float increasingSpeed;
+
+    private int _level;
+
+    private void Awake()
+    {
+        CreateLevel();  
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Time.timeScale = (Time.timeScale == 0f) ? 1f : 0f;
+        }
+
+    }
+
+    private void CreateLevel()
+    {
+        Block block = Lean.Pool.LeanPool.Spawn(standartBlock, transform.position, Quaternion.identity).GetComponent<Block>();
+        block.SetParameters(fallingSpeed + _level * increasingSpeed, transform.position);
+    }
+}
